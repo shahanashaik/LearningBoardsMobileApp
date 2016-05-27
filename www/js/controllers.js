03719360187380d4e7faa92187b1261844458014
shahana <shahana.shaik@accenture.com>
2016-05-27 16:30:53 +0530

    Added code for opening web view via system, external and cordova

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -95,7 +95,24 @@ $scope.goToSearch = function () {
   };
   $scope.start = function(){
     $scope.buttonStyle="button-calm";
-  }
+  };
+  $scope.openInExternalBrowser = function()
+  {
+    // Open in external browser
+    window.open('http://google.com','_system','location=no'); 
+  };
+ 
+  $scope.openInAppBrowser = function()
+  {
+    // Open in app browser
+    window.open('http://google.com','_blank'); 
+  };
+  $scope.openCordovaWebView = function()
+  {
+  // Open cordova webview if the url is in the whitelist otherwise opens in app browser
+    window.open('http://google.com','_self'); 
+  };
+  
 })
 
 .controller('xmlController', function($scope, $http) 
